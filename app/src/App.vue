c46c772b48c61984313eecefa1f2f6ca92f1a01d
Morty Choi <morty.choi@scmp.com>
2019-01-08 15:41:05 +0800

    Revert to files to initial state

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,12 +1,12 @@
-//
-//
-//
-//
-//
-//
-//
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  </div>
+</template>
 
-import Vue from 'vue';
+<script lang="ts">
+import Vue from "vue";
 import HelloWorld from './components/HelloWorld.vue';
 
 export default Vue.extend({
@@ -15,3 +15,15 @@ export default Vue.extend({
     HelloWorld,
   },
 });
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
